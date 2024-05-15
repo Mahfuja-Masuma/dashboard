a916a645e248962321596ec5de59b6a67b3e1d84
Mahfuja Masuma <mahfuzamasuma103@gmail.com>
2024-05-15 09:46:39 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// const sidebarToggle = document.querySelector("#sidebar-toggle")
+// sidebarToggle.addEventListener("click", function(){
+//     document.querySelector("#sidebar").classList.toggle("collapsed");
+// })
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     var sidebarLinks = document.querySelectorAll(".sidebar-link");
+  
+//     sidebarLinks.forEach(function(link) {
+//       link.addEventListener("click", function(event) {
+//         // Remove active class from all sidebar items
+//         document.querySelectorAll('.sidebar-item.active').forEach(function(item) {
+//           item.classList.remove('active');
+//         });
+  
+//         // Add active class to the clicked sidebar item
+//         this.closest(".sidebar-item").classList.add("active");
+//       });
+//     });
+//   });
+
+
+
+
+//   // table content: 
+
+
+
+  
+  
